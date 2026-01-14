d126f1c88517cdc7a6595ba26769049187fddb26
RoniIFSP <roniberto.rodrigues@aluno.ifsp.edu.br>
2026-01-14 15:30:39 -0300

    Primeiro commit Python Santander Academy

---
+++ estruturas_controle.py
@@ -0,0 +1,12 @@
+Idade = 17
+
+if Idade >= 18 and Idade < 60:
+    print ("É um adulto")
+elif Idade == 60:
+    print ("Parabéns pelos 60 anos!!!")
+elif Idade > 15 and Idade < 18:
+    print ("Tem mais de 15 anos.")
+elif Idade < 18:
+    print ("É menor de idade.")
+else:
+    print ("É um cidadão idoso.")
---
+++ loops.py
@@ -0,0 +1,9 @@
+print ("Números de 1 a 5 multiplicados por 2  com o loop for:")
+for numero in range (1, 6):
+    print(numero * 2)
+
+print ("Números de 1 a 5 multiplicados por 2  com o loop while:")
+contador = 1
+while contador <=5:
+    print(contador * 2)
+    contador += 1
