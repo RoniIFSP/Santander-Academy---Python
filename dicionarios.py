72d3088a947ab96b5f9ff56694614a6eba8b4a16
RoniIFSP <roniberto.rodrigues@aluno.ifsp.edu.br>
2026-01-15 18:38:40 -0300

    dicionarios em Python

---
+++ dicionarios.py
@@ -0,0 +1,5 @@
+pessoa = {"nome":"Roniberto", "idade":26, "cidade":"São Paulo"}
+
+print(pessoa["nome"])
+print(pessoa["idade"])
+print(pessoa["cidade"])
---
+++ metodos_dicionarios.py
@@ -0,0 +1,13 @@
+pessoa = {"nome":"Roniberto", "idade":26, "cidade":"São Paulo"}
+
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+
+print("")
+
+print(pessoa.update({"prifissao":"Desenvolvedor"}))
+
+print("")
+
+print(pessoa)
