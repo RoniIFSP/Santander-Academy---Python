b0e0a2b411de960c0c12706dba2b4fcd30eb9874
RoniIFSP <roniberto.rodrigues@aluno.ifsp.edu.br>
2026-01-15 18:07:17 -0300

    Listas em Python

---
+++ listas.py
@@ -0,0 +1,11 @@
+frutas = ["maça", "banana", "laranja"]
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+
+print("")
+
+print(frutas[-1])
+print(frutas[-2])
+print(frutas[-3])
---
+++ metodos_listas.py
@@ -0,0 +1,30 @@
+frutas = ["maça", "banana", "laranja"]
+
+frutas.append("pera")
+print(frutas)
+
+print("")
+
+frutas.insert(1,"uva")
+print(frutas)
+
+print("")
+
+frutas.remove("banana")
+print(frutas)
+
+print("")
+
+fruta_removida = frutas.pop(2)
+print(frutas)
+print(fruta_removida)
+
+print("")
+
+frutas.sort()
+print(frutas)
+
+print("")
+
+frutas.reverse()
+print(frutas)
