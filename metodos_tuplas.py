8481c68b5beebf09da783a702bc89256239f048c
RoniIFSP <roniberto.rodrigues@aluno.ifsp.edu.br>
2026-01-15 18:28:17 -0300

    tuplas em Python

---
+++ metodos_tuplas.py
@@ -0,0 +1,15 @@
+minha_tupla = (1, 2, 3, 2, 4, 2)
+
+print(minha_tupla.index(2))
+print(minha_tupla.index(2, 2))
+print(minha_tupla.index(2, 2, 4))
+
+print("")
+
+print(minha_tupla.count(2))
+print(minha_tupla.count(4))
+print(minha_tupla.count(10))
+
+print("")
+
+print(len(minha_tupla))
