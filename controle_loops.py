e3322540a9bd844461177ee8d6558f40b216f738
RoniIFSP <roniberto.rodrigues@aluno.ifsp.edu.br>
2026-01-15 17:37:33 -0300

    Inclusão do controle de loops

---
+++ controle_loops.py
@@ -0,0 +1,20 @@
+contador = 0
+
+while True:
+    print (contador)
+    contador += 1
+
+    if contador == 5:
+        break
+
+print("")
+
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+print("")
+
+for x in range (5):
+    pass
